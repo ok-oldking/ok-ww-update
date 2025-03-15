06f40c6336653dbd5666fd7889e0f17a662886cf
ok-oldking <ok-oldking@users.noreply.github.com>
2025-03-15 06:23:09 +0000

    修复可能领不到大月卡任务

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import numpy as np
 
 from ok import ConfigOption
 
-version = "v2.1.19"
+version = "v2.1.20"
 
 
 def calculate_pc_exe_path(running_path):
@@ -72,6 +72,7 @@ config = {
         'default_horizontal_variance': 0.002,
         'default_vertical_variance': 0.002,
         'default_threshold': 0.8,
+        'target_height': 540,
     },
     'windows': {  # required  when supporting windows game
         'exe': 'Client-Win64-Shipping.exe',
