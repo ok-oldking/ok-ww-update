0f9795ac19420da95c478254e1f15ac5f5d446cf
ok-oldking <ok-oldking@users.noreply.github.com>
2025-06-25 10:45:46 +0000

    优化卡提、风主 @BnanZ0

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import numpy as np
 
 from ok import ConfigOption, get_path_relative_to_exe
 
-version = "v2.4.34"
+version = "v2.4.36"
 
 
 def calculate_pc_exe_path(running_path):
@@ -48,10 +48,10 @@ def make_bottom_right_black(frame):
 
 
 key_config_option = ConfigOption('Game Hotkey Config', {
-    'Auto Set HotKey': False,
     'Echo Key': 'q',
     'Liberation Key': 'r',
     'Resonance Key': 'e',
+    'Tool Key': 't',
 }, description='In Game Hotkey for Skills')
 
 pick_echo_config_option = ConfigOption('Pick Echo Config', {
