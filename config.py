65904dd38379d9b727e8a3b739f7e0c0595affe8
ok-oldking <ok-oldking@users.noreply.github.com>
2025-07-28 04:17:07 +0000

    1.修复转圈奔跑不按F 2.优化捡声骸效率 (RU)

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import numpy as np
 from ok import ConfigOption
 from src.task.process_feature import process_feature
 
-version = "v2.5.9"
+version = "v2.5.10"
 
 
 def calculate_pc_exe_path(running_path):
