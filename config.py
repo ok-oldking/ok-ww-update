4e240b116b7e611a0009c48c66c262f7186e3c9f
ok-oldking <ok-oldking@users.noreply.github.com>
2026-01-01 15:42:06 +0000

    优化点击F (ok-oldking) 优化日常一条龙 (RU)

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import numpy as np
 from ok import ConfigOption
 from src.task.process_feature import process_feature
 
-version = "v3.0.7"
+version = "v3.0.8"
 
 
 def calculate_pc_exe_path(running_path):
