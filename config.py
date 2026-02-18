73260ba1c18c91bdafb94e4ffc796cc2d6a16c57
ok-oldking <ok-oldking@users.noreply.github.com>
2026-02-18 14:28:15 +0000

    修复云游戏战斗识别问题 (ok-oldking) 优化体力识别 (ok-oldking)

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import numpy as np
 from ok import ConfigOption
 from src.task.process_feature import process_feature
 
-version = "v3.1.16"
+version = "v3.1.17"
 
 
 def calculate_pc_exe_path(running_path):
