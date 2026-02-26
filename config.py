b39af945d6d0313f538d668cc676dfd1e67be0b7
ok-oldking <ok-oldking@users.noreply.github.com>
2026-02-26 15:29:24 +0000

    优化强化声骸属性识别 (ok-oldking) 自动强化首条双爆仅检查一次 (ok-oldking)

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ import numpy as np
 from ok import ConfigOption
 from src.task.process_feature import process_feature
 
-version = "v3.1.34"
+version = "v3.1.36"
 
 
 def calculate_pc_exe_path(running_path):
