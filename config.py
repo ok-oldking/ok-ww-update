2c192b4ddb1afc470b16ee6edff3df1f0448b54e
ok-oldking <ok-oldking@users.noreply.github.com>
2026-06-04 15:04:17 +0000

    签名exe (ok-oldking)

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from qfluentwidgets import FluentIcon
 from ok import Box, ConfigOption
 from src.task.process_feature import process_feature
 
-version = "v3.3.72"
+version = "v3.3.73"
 
 
 def calculate_pc_exe_path(running_path):
