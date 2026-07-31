4f12472f3a4366690760ca4c30d3ece6de7fba7d
ok-oldking <ok-oldking@users.noreply.github.com>
2026-07-31 14:45:10 +0000

    fix tests (ok-oldking) 支持21:9分辨率 (ok-oldking) 兼容21:9屏幕 (ok-oldking) 优化穗穗 (ok-oldking)

--- src/char/Suisui.py
+++ src/char/Suisui.py
@@ -5,7 +5,6 @@ from src.char.BaseChar import BaseChar, SwitchPriority
 
 
 class Suisui(BaseChar):
-    ATTACK_DURATION = 1.2
     ATTACK_INTERVAL = 0.1
     FORTE_TIMEOUT = 26.0
     CONCERTO_TIMEOUT = 12.0
@@ -32,42 +31,50 @@ class Suisui(BaseChar):
             if self.forte3_available():
                 self.logger.debug('forte3 available')
                 break
-            self.continues_normal_attack(
-                self.ATTACK_DURATION,
-                interval=self.ATTACK_INTERVAL,
-                click_resonance_if_ready_and_return=True,
-            )
+            self.cycle_start()
+            self.do_cycle()
+            self.cycle_sleep(self.ATTACK_INTERVAL)
             self.task.next_frame()
         else:
             self.logger.warning('Suisui forte3 detection timed out')
             return
 
-        self._lock_after_switch = True
-        self.click_liberation(wait_if_cd_ready=0)
-
+        liberation_clicked = False
         start = time.time()
         while self.time_elapsed_accounting_for_freeze(start) < self.CONCERTO_TIMEOUT:
+            if not liberation_clicked:
+                liberation_clicked = self.click_liberation(wait_if_cd_ready=0)
+                if not liberation_clicked:
+                    self.cycle_start()
+                    self.do_cycle()
+                    self.cycle_sleep(self.ATTACK_INTERVAL)
+                    self.task.next_frame()
+                    continue
             if self.is_con_full():
                 return
-            self.continues_normal_attack(
-                self.ATTACK_DURATION,
-                interval=self.ATTACK_INTERVAL,
-                click_resonance_if_ready_and_return=True,
-            )
+            self.cycle_start()
+            self.do_cycle()
+            self.cycle_sleep(self.ATTACK_INTERVAL)
             self.task.next_frame()
         self.logger.warning('Suisui concerto fill timed out')
+
+    def do_cycle(self):
+        if self.resonance_available():
+            return self.click_resonance()
+        self.click()
 
     def forte3_available(self):
         return bool(self.task.find_one(Labels.suisui_forte3, threshold=0.6))
 
     def switch_out(self, con_full=False):
-        lock_after_switch = self._lock_after_switch
-        self._lock_after_switch = False
         super().switch_out(con_full=con_full)
-        if lock_after_switch:
+        if con_full:
             self.last_forte3_switch = time.time()
 
     def get_switch_priority(self, current_char=None, has_intro=False, target_low_con=False):
+        since_last = self.time_elapsed_accounting_for_freeze(self.last_forte3_switch)
+        if since_last > 40:
+            return SwitchPriority.MUST
         has_main_dps = any(
             char and char != self and char.is_main_dps
             for char in getattr(self.task, 'chars', [])
@@ -77,7 +84,7 @@ class Suisui(BaseChar):
             if has_main_dps
             else self.FORTE3_SWITCH_LOCKOUT
         )
-        if self.time_elapsed_accounting_for_freeze(self.last_forte3_switch) < switch_lockout:
+        if since_last < switch_lockout:
             return SwitchPriority.NO
         if has_main_dps:
             if current_char and current_char.is_main_dps and has_intro:
