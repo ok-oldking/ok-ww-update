7c579d95b3e51aec5005075cd3d8dcf1036fcddb
ok-oldking <ok-oldking@users.noreply.github.com>
2026-08-16 03:05:46 +0000

    update test dependencies and remove web tests (ok-oldking) update build (ok-oldking) add web for testing (ok-oldking) update for web (ok-oldking) Update AI triage and pull request review (ok-oldking)

--- config.py
+++ config.py
@@ -2,15 +2,10 @@ import os
 import re
 from pathlib import Path
 
-# WA: set empty PATH to resolve qfluentwidgets/PySide6 access os.environ['PATH'] issue
-if 'PATH' not in os.environ:
-    os.environ['PATH'] = ""
-from qfluentwidgets import FluentIcon
-
-from ok import Box, ConfigOption
+from ok import Box, ConfigOption, Icon
 from src.task.process_feature import process_feature
 
-version = "v3.5.28-beta.5"
+version = "v3.5.28-beta.7"
 
 
 def _find_most_recently_run_pc_exe():
@@ -139,12 +134,12 @@ key_config_option = ConfigOption('Game Hotkey', {
     'Bag Key': 'b',
 }, description='In Game Hotkey for Skills', config_description={
     'Bag Key': 'In-game hotkey used to open the Bag.',
-}, show_at_tab=True, icon=FluentIcon.GAME)
+}, show_at_tab=True, icon=Icon.GAME)
 
 char_config_option = ConfigOption('Character Config', {
     'Iuno C6': False,
     'Chisa DPS': False,
-}, description='Character Config', show_at_tab=True, icon=FluentIcon.PEOPLE)
+}, description='Character Config', show_at_tab=True, icon=Icon.PEOPLE)
 
 monthly_card_config_option = ConfigOption('Monthly Card Config', {
     'Check Monthly Card': True,
@@ -157,7 +152,9 @@ monthly_card_config_option = ConfigOption('Monthly Card Config', {
 config = {
     'debug': False,  # Optional, default: False
     'custom_tasks': True,
-    'use_gui': True,
+    "gui": {
+        "type": "qt",
+    },
     'config_folder': 'configs',
     'blur_area': blur_area,
     'gui_icon': 'icons/icon.png',
@@ -220,15 +217,15 @@ config = {
             'github': 'https://github.com/ok-oldking/ok-wuthering-waves',
             'discord': 'https://discord.gg/vVyCatEBgA',
             'sponsor': 'https://patreon.com/ok_oldking',
-            'share': 'Download OK-WW from https://github.com/ok-oldking/ok-wuthering-waves/releases/latest',
-            'faq': 'https://ok-oldking.github.io/ok-wuthering-waves/en/'
+            'share': 'Download OK-WW from https://ok-script.com/ok-ww',
+            'faq': 'https://ok-script.com/ok-ww/'
         },
         'zh_CN': {
             'github': 'https://github.com/ok-oldking/ok-wuthering-waves',
             'discord': 'https://discord.gg/vVyCatEBgA',
             'sponsor': 'https://afdian.com/a/ok-oldking',
-            'share': 'GitHub: https://github.com/ok-oldking 百度网盘: https://pan.baidu.com/s/102Mh1djq2B1T-cIJhct9Gg?pwd=okww 夸克网盘: https://pan.quark.cn/s/d080413c87ac Mirror酱：https://mirrorchyan.com/zh/projects?source=okbilibili',
-            'faq': 'https://ok-oldking.github.io/ok-wuthering-waves/zh-CN/',
+            'share': '下载okww https://ok-script.com/ok-ww',
+            'faq': 'https://ok-script.com/ok-ww',
             'qq_group': 'https://qm.qq.com/q/SUQpIpmq4',
             'qq_channel': 'https://pd.qq.com/s/djmm6l44y',
         },
